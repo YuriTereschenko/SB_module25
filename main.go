db2ef78cf185d1cece16bc2ef7a404075022cdcd
Yurii Tereschenko <yu0303@yandex.ru>
2023-03-17 12:49:19 +0500

    fix

--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	var str, subStr string
 	flag.StringVar(&str, "str", "None", "Set str")
 	flag.StringVar(&subStr, "substr", "None", "Set substr")
+	flag.Parse()
 	fmt.Println(strings.Contains(str, subStr))
 }
