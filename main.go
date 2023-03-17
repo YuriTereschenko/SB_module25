7f066c3bb0b2d37b67ef08dac92be2fd45505dd7
Yurii Tereschenko <yu0303@yandex.ru>
2023-03-17 12:46:31 +0500

    task done

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str, subStr string
+	flag.StringVar(&str, "str", "None", "Set str")
+	flag.StringVar(&subStr, "substr", "None", "Set substr")
+	fmt.Println(strings.Contains(str, subStr))
+}
